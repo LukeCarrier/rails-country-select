cd0bc839c6fd7f4fcb18a693920a22ea74de3ecc
Luke Carrier <hey@lukecarrier.me>
2011-09-04 18:37:52 +0100

    Whoops, remove logging; it's all fixed now

--- lib/country_helper.rb
+++ lib/country_helper.rb
@@ -8,7 +8,6 @@ module ActionView
       def country_select(object, method, options = {}, html_options = {})
         options[:keys] = :names unless options.has_key?(:keys)
         options[:values]  = :nums  unless options.has_key?(:values)
-        Rails.logger.info("keys: " << options[:keys].to_s << " values: " << options[:values].to_s)
 
         potential = {
           names:   COUNTRY_NAMES,
@@ -16,7 +15,6 @@ module ActionView
           alpha2s: COUNTRY_ALPHA2S,
           alpha3s: COUNTRY_ALPHA3S
         }
-        Rails.logger.info("keys: " << potential[options[:keys]].to_s << " values: " << potential[options[:values]].to_s)
 
         select_options = potential[options[:keys]].zip(potential[options[:values]])
 
