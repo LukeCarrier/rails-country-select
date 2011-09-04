ae8adcb130f642b369e71fb5244ec9e637f0e99b
Luke Carrier <hey@lukecarrier.me>
2011-09-04 18:20:42 +0100

    Update the tests to reflect the new constant names

--- test/country_test.rb
+++ test/country_test.rb
@@ -13,15 +13,15 @@ end
 
 class CountryTest < Test::Unit::TestCase
   def test_country_definitition_lists_are_of_equal_length
-    assert_equal COUNTRY_NAMES.length, COUNTRY_ALPHA2.length
-    assert_equal COUNTRY_NAMES.length, COUNTRY_ALPHA3.length
-    assert_equal COUNTRY_NAMES.length, COUNTRY_NUM.length
+    assert_equal COUNTRY_NAMES.length, COUNTRY_ALPHA2S.length
+    assert_equal COUNTRY_NAMES.length, COUNTRY_ALPHA3S.length
+    assert_equal COUNTRY_NAMES.length, COUNTRY_NUMS.length
   end
 
   def test_country_definition_lists_contain_no_duplicates
     assert_equal 0, COUNTRY_NAMES.only_duplicates.length
-    assert_equal 0, COUNTRY_ALPHA2.only_duplicates.length
-    assert_equal 0, COUNTRY_ALPHA3.only_duplicates.length
-    assert_equal 0, COUNTRY_NUM.only_duplicates.length
+    assert_equal 0, COUNTRY_ALPHA2S.only_duplicates.length
+    assert_equal 0, COUNTRY_ALPHA3S.only_duplicates.length
+    assert_equal 0, COUNTRY_NUMS.only_duplicates.length
   end
 end
