bdaec5f5e45ad3da30dc6a6c1387f6098bd9be41
Luke Carrier <hey@lukecarrier.me>
2011-09-04 18:34:41 +0100

    Allow overriding the keys and values for the <option>s via the :options hash

--- lib/country_helper.rb
+++ lib/country_helper.rb
@@ -6,14 +6,26 @@ module ActionView
   module Helpers
     module FormOptionsHelper
       def country_select(object, method, options = {}, html_options = {})
-        InstanceTag.new(object, method, self, options.delete(:object)).to_select_tag(COUNTRY_NAMES, options, html_options)
+        options[:keys] = :names unless options.has_key?(:keys)
+        options[:values]  = :nums  unless options.has_key?(:values)
+        Rails.logger.info("keys: " << options[:keys].to_s << " values: " << options[:values].to_s)
+
+        potential = {
+          names:   COUNTRY_NAMES,
+          nums:    COUNTRY_NUMS,
+          alpha2s: COUNTRY_ALPHA2S,
+          alpha3s: COUNTRY_ALPHA3S
+        }
+        Rails.logger.info("keys: " << potential[options[:keys]].to_s << " values: " << potential[options[:values]].to_s)
+
+        select_options = potential[options[:keys]].zip(potential[options[:values]])
+
+        InstanceTag.new(object, method, self, options.delete(:object)).to_select_tag(select_options, options, html_options)
       end
     end
 
     class FormBuilder
       def country_select(method, options = {}, html_options = {})
-        Rails.logger.info("ActionView::Helpers::FormBuilder.country_select()")
-        Rails.logger.info([method, options, html_options].to_s)
         @template.country_select(@object_name, method, options.merge({object: @object}), html_options)
       end
     end
